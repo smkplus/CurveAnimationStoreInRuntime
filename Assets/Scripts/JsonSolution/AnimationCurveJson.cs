d4fcf0a49821000db8b2c0daab5f1547ce24e9f7
Seyed Morteza Kamali <smkplus3d@gmail.com>
2020-11-06 11:26:22 +0330

    serialize json

---
+++ Assets/Scripts/JsonSolution/AnimationCurveJson.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AnimationCurveJson 
+{
+    public AnimationCurve animationCurve;
+
+    public SaveObject saveObject = new SaveObject();
+    
+    public void AddKey(Keyframe keyframe)
+    {
+        animationCurve.AddKey(keyframe);
+        saveObject.AddKey(keyframe);
+    }
+
+    public float GetKeyAt(float time)
+    {
+        return animationCurve.Evaluate(time);
+    }
+}
+[System.Serializable]
+public class SaveObject
+{
+
+    public List<float> Time = new List<float>();
+    public List<float> Value = new List<float>();
+        
+    public void AddKey(Keyframe keyframe)
+    {
+        Time.Add(keyframe.time);   
+        Value.Add(keyframe.value);   
+    }
+}
---
+++ Assets/Scripts/JsonSolution/AnimationCurveJsonSaver.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Linq;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class AnimationCurveJsonSaver : MonoBehaviour
+{
+    [SerializeField] private bool isRecording;
+
+    [SerializeField] private Transform target;
+    [SerializeField] private AnimationCurveJson[] animationCurveJson;
+
+
+    private void Awake()
+    {
+        if (!isRecording)
+        {
+            LoadJson();
+            print("Load Successfully");
+        }
+    }
+
+    private void Update()
+    {
+        if (isRecording)
+        {
+            RecordPosition();
+        }
+        else
+        {
+            LoadPosition();
+        }
+    }
+    
+    private void RecordPosition()
+    {
+        var position = target.position;
+        animationCurveJson[0].AddKey(new Keyframe(Time.time, position.x));
+        animationCurveJson[1].AddKey(new Keyframe(Time.time, position.y));
+        animationCurveJson[2].AddKey(new Keyframe(Time.time, position.z));
+    }
+    
+    private void LoadPosition()
+    {
+        print(animationCurveJson[0].GetKeyAt(Time.time));
+        target.position = new Vector3(
+            animationCurveJson[0].GetKeyAt(Time.time),
+            animationCurveJson[1].GetKeyAt(Time.time),
+            animationCurveJson[2].GetKeyAt(Time.time)
+        );
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (isRecording)
+        {
+            SaveJson();
+            print("Saved Successfully");
+        }
+    }
+
+    private void SaveJson()
+    {
+        print(Application.persistentDataPath + "/save.txt");
+        var saveObjects = animationCurveJson.Select(x => x.saveObject).ToArray();
+        var json = JsonHelper.ToJson(saveObjects, true);
+        File.WriteAllText(Application.persistentDataPath + "/save.txt", json);
+    }
+
+    private void LoadJson()
+    {
+        var data = File.ReadAllText(Application.persistentDataPath + "/save.txt");
+        var json = JsonHelper.FromJson<SaveObject>(data);
+        for (int index = 0; index < animationCurveJson.Length; index++)
+        {
+            for (var i = 0; i < json[index].Time.Count; i++)
+            {
+                animationCurveJson[index].AddKey(new Keyframe(json[index].Time[i], json[index].Value[i]));
+            }
+        }
+    }
+}
